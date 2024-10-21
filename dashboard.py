864976c85278e4cd50fd61f572656c1e1cd22ee9
Frederic Menezes Ferreira <fredericmferreira@gmail.com>
2024-10-21 11:14:08 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -6,8 +6,6 @@ import seaborn as sns
 # Carregar o dataset
 df = pd.read_csv('house02.csv')
 
-
-
 # Mapeamento das opções de 'furnished' para português
 furniture_map = {
     'furnished': 'mobiliado',
@@ -16,8 +14,8 @@ furniture_map = {
 
 # Mapeamento das opções de 'accept' para português
 accept_map = {
-    'acept': 'aceita',
-    'not acept': 'não aceita'
+    'accept': 'aceita',
+    'not accept': 'não aceita'
 }
 
 # Título do dashboard
@@ -33,48 +31,54 @@ dados = df[df['city'] == cidade]
 q = dados["area"].quantile(0.99)
 df_filtered = dados[(dados["area"] < q)]
 
+# Criar duas colunas para espalhar os gráficos
+col1, col2 = st.columns(2)
+
 # Gráfico 1: Distribuição dos valores de aluguel na cidade selecionada
-st.subheader(f'Distribuição de Valores de Aluguel em {cidade}')
-fig, ax = plt.subplots()
-sns.histplot(df_filtered['rent amount (R$)'], kde=True, ax=ax)
-ax.set_xlabel('Valor do Aluguel (R$)')
-ax.set_ylabel('Frequência')
-st.pyplot(fig)
+with col1:
+    st.subheader(f'Distribuição de Valores de Aluguel em {cidade}')
+    fig, ax = plt.subplots()
+    sns.histplot(df_filtered['rent amount (R$)'], kde=True, ax=ax)
+    ax.set_xlabel('Valor do Aluguel (R$)')
+    ax.set_ylabel('Frequência')
+    st.pyplot(fig)
 
 # Gráfico 2: Relação entre área do imóvel e valor do aluguel
-st.subheader(f'Relação entre Área e Valor do Aluguel em {cidade}')
-# Mapeamento dos valores da coluna 'furniture' para português no dataframe filtrado
-df_filtered.loc[:, 'Mobilia:'] = df_filtered['furniture'].map(furniture_map)
-# Mapeamento dos valores da coluna 'accept' para português no dataframe filtrado
-df_filtered.loc[:, 'Animais:'] = df_filtered['animal'].map(accept_map)
-fig, ax = plt.subplots()
-# sns.scatterplot(data=df_filtered, x='area', y='rent amount (R$)', hue='furniture', style='animal', ax=ax)
-sns.scatterplot(data=df_filtered, x='area', y='rent amount (R$)', hue='Mobilia:', style='Animais:', ax=ax)
-ax.set_xlabel('Área (m²)')
-ax.set_ylabel('Valor do Aluguel (R$)')
-st.pyplot(fig)
+with col2:
+    st.subheader(f'Relação entre Área e Valor do Aluguel em {cidade}')
+    df_filtered.loc[:, 'Mobilia:'] = df_filtered['furniture'].map(furniture_map)
+    df_filtered.loc[:, 'Animais:'] = df_filtered['animal'].map(accept_map)
+    fig, ax = plt.subplots()
+    sns.scatterplot(data=df_filtered, x='area', y='rent amount (R$)', hue='Mobilia:', style='Animais:', ax=ax)
+    ax.set_xlabel('Área (m²)')
+    ax.set_ylabel('Valor do Aluguel (R$)')
+    st.pyplot(fig)
+
+# Criar mais duas colunas para os próximos gráficos
+col3, col4 = st.columns(2)
 
 # Gráfico 3: Comparação de imóveis mobiliados e não mobiliados
-st.subheader('Comparação de Imóveis Mobiliados vs Não Mobiliados')
-# furnished_option = st.radio("Escolha o tipo de imóvel", ('furnished', 'not furnished'))
-furnished_option_pt = st.radio("Escolha o tipo de imóvel", ('mobiliado', 'não mobiliado'))
+with col3:
+    st.subheader('Comparação de Imóveis Mobiliados vs Não Mobiliados')
+    furnished_option_pt = st.radio("Escolha o tipo de imóvel", ('mobiliado', 'não mobiliado'))
 
-# Traduzir de volta para o valor original do dataframe
-furnished_option = [k for k, v in furniture_map.items() if v == furnished_option_pt][0]
-df_furnished = df[df['furniture'] == furnished_option]
-fig, ax = plt.subplots()
-sns.boxplot(data=df_furnished, x='city', y='rent amount (R$)', ax=ax)
-ax.set_xlabel('Cidade')
-ax.set_ylabel('Valor do Aluguel (R$)')
-st.pyplot(fig)
+    # Traduzir de volta para o valor original do dataframe
+    furnished_option = [k for k, v in furniture_map.items() if v == furnished_option_pt][0]
+    df_furnished = df[df['furniture'] == furnished_option]
+    fig, ax = plt.subplots()
+    sns.boxplot(data=df_furnished, x='city', y='rent amount (R$)', ax=ax)
+    ax.set_xlabel('Cidade')
+    ax.set_ylabel('Valor do Aluguel (R$)')
+    st.pyplot(fig)
 
 # Gráfico 4: Número de vagas de estacionamento em relação ao aluguel
-st.subheader(f'Vagas de Estacionamento x Valor de Aluguel em {cidade}')
-fig, ax = plt.subplots()
-sns.boxplot(data=df_filtered, x='parking spaces', y='rent amount (R$)', ax=ax)
-ax.set_xlabel('Número de Vagas de Estacionamento')
-ax.set_ylabel('Valor do Aluguel (R$)')
-st.pyplot(fig)
+with col4:
+    st.subheader(f'Vagas de Estacionamento x Valor de Aluguel em {cidade}')
+    fig, ax = plt.subplots()
+    sns.boxplot(data=df_filtered, x='parking spaces', y='rent amount (R$)', ax=ax)
+    ax.set_xlabel('Número de Vagas de Estacionamento')
+    ax.set_ylabel('Valor do Aluguel (R$)')
+    st.pyplot(fig)
 
 # Conclusões
 st.subheader("Conclusões")
